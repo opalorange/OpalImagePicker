20c91fd93d4aff8c9742567435ac51b79313f63a
Roland Lindner <roland.lindner@hausgold.de>
2019-08-07 15:23:34 +0200

    Add @objc attributes to the configurations properties

--- OpalImagePicker/Source/OpalImagePickerConfiguration.swift
+++ OpalImagePicker/Source/OpalImagePickerConfiguration.swift
@@ -14,36 +14,36 @@ public class OpalImagePickerConfiguration: NSObject {
     var updateStrings: ((OpalImagePickerConfiguration) -> Void)?
     
     /// Localized navigation title. Defaults to "Photos".
-    public var navigationTitle: String? {
+    @objc public var navigationTitle: String? {
         didSet {
             updateStrings?(self)
         }
     }
     
     /// Localized library segment title. Only displays when using external URLs in `UISegmentedControl`.
-    public var librarySegmentTitle: String? {
+    @objc public var librarySegmentTitle: String? {
         didSet {
             updateStrings?(self)
         }
     }
     
     /// Localized 'Cancel' button text.
-    public var cancelButtonTitle: String? {
+    @objc public var cancelButtonTitle: String? {
         didSet {
             updateStrings?(self)
         }
     }
     
     /// Localized 'Done' button text.
-    public var doneButtonTitle: String? {
+    @objc public var doneButtonTitle: String? {
         didSet {
             updateStrings?(self)
         }
     }
     
     /// Localized maximum selections allowed error message displayed to the user.
-    public var maximumSelectionsAllowedMessage: String?
+    @objc public var maximumSelectionsAllowedMessage: String?
     
     /// Localized "OK" string.
-    public var okayString: String?
+    @objc public var okayString: String?
 }
